1b2c93f36efd14009339742eaa34ed96c8f5d523
Ru-Ri-z <69484119+Ru-Ri-z@users.noreply.github.com>
2021-07-06 18:17:51 -0300

    Add files via upload

---
+++ agregados.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.header--title-container', { delay: 250});
+ScrollReveal().reveal('.main-exchange-container', { delay: 500});
+ScrollReveal().reveal('.main-product-detail', { delay: 750});
+ScrollReveal().reveal('.bitcoin-img-container', { delay: 1000});
+ScrollReveal().reveal('.main-plans-container', { delay: 1250});
+ScrollReveal().reveal('.left', { delay: 1500});
+ScrollReveal().reveal('.right', { delay: 1750});
+
